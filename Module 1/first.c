a2dd3202ad94cbdd6fe7cf0f5f979efbaf1e4c20
RAHUL <rahulsd380@gmail.com>
2025-04-06 20:56:24 +0600

    Explored input, print function in c

---
+++ Module 1/first.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main () 
+{
+    // printf("Hello\n");
+    // printf("There\t");
+    // printf("This is me!!");
+    int num1 = 10;
+    char firstName = 'Rahul';
+    float f = 2.4;
+    // printf("%d", num1);
+    printf("%c", firstName);
+    printf("%.2f", f);    // %.2 is used to indicate 2 decimal points in float value
+    return 0;
+}
+
+
+
+
+
+
+
+// Notes : 
+// 1. \n is for new line
+// 2. \t is for new tab
+// 3. #include<stdio.h> is header file which containe all the commands to excute the printf and other functions
+// 4. int is used to declare integer type variable
+// 5. char is used to declare character type variable
+// 6. float is used to declare floating point variable
+// 7. int will take 4 bytes of memory
+// 8. char will take 1 byte of memory
+// 9. float will take 4 bytes of memory
+
+
+// To print any variable we have to use format specifier :
+// Format Specifier
+// int - %d
+// char - %c
+// float - %f
+
+
+// 1 bit = 8byte
+// 1 byte = 1024 bits
+// 1 KB = 1024 bytes
+// 1 MB = 1024 KB
+// 1 GB = 1024 MB
+// 1 TB = 1024 GB
+// 1 PB = 1024 TB
---
+++ Module 1/input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int num1;
+    scanf("%d", &num1);
+    printf("%d", num1);
+    return 0;
+};
+
+// int can contain 10^9 value
+// long long int can contain 10^18 value

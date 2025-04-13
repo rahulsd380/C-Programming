96670ecc2139cbcb6c40755b4de21b0a46b7304c
RAHUL <rahulsd380@gmail.com>
2025-04-13 15:25:39 +0600

    Conditionals

---
+++ Module 4/lecture-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int A;
+    int B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    if(A >= B){
+        printf("YES");
+    } else {
+        printf("NO");
+    };
+    return 0;
+}

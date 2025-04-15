2d037af7020908b1878532b071393fe27ce93023
RAHUL <rahulsd380@gmail.com>
2025-04-15 15:44:43 +0600

    Learning pre and post increment decrement

---
+++ Module 5/pre-and-post-increment-decrement.c
@@ -0,0 +1,25 @@
+// #include<stdio.h>
+// int main() {
+//     int x = 10;
+//     int y = x++;
+//     printf("%d %d", x,y);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main() {
+//     int x = 10;
+//     ++x;
+//     printf("%d", x++);
+//     return 0;
+// }
+
+#include<stdio.h>
+int main() {
+    int x = 10;
+    int y = x++;
+    int z = ++y;
+    printf("%d", z++);
+    return 0;
+}
+

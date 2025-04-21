9e4b66b28ca96b43590ce42001970f9acbc72858
RAHUL <rahulsd380@gmail.com>
2025-04-21 10:17:34 +0600

    Leangth of string printing

---
+++ Module 8/leangth-of-string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    char a[50];
+    scanf("%s", a);
+    int count = 0;
+    for(int i=0; a[i] != '\0'; i++){
+        count++;
+    }
+    printf("%d", count);
+    return 0;
+}

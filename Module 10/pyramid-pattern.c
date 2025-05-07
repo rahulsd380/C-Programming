51b4f2e9d65bf6fe77a5ce87bb51069520bef22f
RAHUL <rahulsd380@gmail.com>
2025-05-07 08:24:41 +0600

    Learning pyramid pattern

---
+++ Module 10/pyramid-pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int star =1;
+    int space = n-1;
+    for(int i = 1; i <= n; i++) {                   //for printing lines
+        for(int k=1; k<=space; k++){                //for printing spaces
+            printf(" ");
+        }
+        for(int j = 1; j <= star; j++) {            //for printing stars
+            printf("*");
+        }
+        printf("\n");
+        star+=2;
+        space--;
+    }
+    return 0;
+}

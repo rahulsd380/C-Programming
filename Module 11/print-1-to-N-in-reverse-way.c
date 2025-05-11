3c81f7d4a202f1ba843a80a83e18ab3733b830fd
RAHUL <rahulsd380@gmail.com>
2025-05-11 22:46:57 +0600

    Printing array elements using recursion

---
+++ Module 11/print-1-to-N-in-reverse-way.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void hello(int i) {
+    if(i == 0) return;
+    printf("%d\n", i);
+    hello(i-1);
+}
+int main() {
+    int i;
+    scanf("%d", &i);
+    hello(i);
+    return 0;
+}

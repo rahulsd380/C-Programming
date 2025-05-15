060282b5f591dd79341abfde2d97d61c73fc3c10
RAHUL <rahulsd380@gmail.com>
2025-05-15 14:58:20 +0600

    checking row metrics

---
+++ Module 12/cking-row-metrics.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if(r == 1) {
+        printf("This is a row metrics.");
+    } else {
+        printf("This is not a row metrics.");
+    }
+    return 0;
+}
